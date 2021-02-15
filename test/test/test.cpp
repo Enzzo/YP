af38d5b9d5b175c3c89d9812d9f6f36e9543b0d2
Sergey <enzzomatrix@gmail.com>
2021-02-15 17:30:17 +1100

    upd

---
+++ test/test/test.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+
+int main(){
+
+    std::string query;
+    std::string word;
+
+    std::getline(std::cin, query);
+
+    for (size_t i = 0; i < query.size(); i++) {
+        if (isspace(query[i])){
+            std::cout << "["<<word<<"]" << std::endl;
+            word = "";
+        }
+        else {
+            word += query[i];
+        }
+    }
+    std::cout << "[" << word << "]" << std::endl;
+
+    return 0;
+}
