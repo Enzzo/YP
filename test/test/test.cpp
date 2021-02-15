ae1013593ace9f92f59f1f6f69ed588350dfdb4f
Sergey <enzzomatrix@gmail.com>
2021-02-16 06:53:14 +1100

    upd

--- test/test/test.cpp
+++ test/test/test.cpp
@@ -1,23 +1,55 @@
 ﻿#include <iostream>
 #include <string>
+#include <vector>
 
-int main(){
+int main() {
+	int q;
+	std::cin >> q;
 
-    std::string query;
-    std::string word;
+	std::vector<bool> query;
 
-    std::getline(std::cin, query);
+	for (int i = 0; i < q; ++i) {
+		std::string operation_code;
+		std::cin >> operation_code;
 
-    for (size_t i = 0; i < query.size(); i++) {
-        if (isspace(query[i])){
-            std::cout << "["<<word<<"]" << std::endl;
-            word = "";
-        }
-        else {
-            word += query[i];
-        }
-    }
-    std::cout << "[" << word << "]" << std::endl;
+		//WORRY_COUNT: узнать количество беспокоящихся людей в очереди.
+		if (operation_code == "WORRY_COUNT") {
+			int count = 0;
+			for (const bool b : query) {
+				count += b;
+			}
+			std::cout << count << std::endl;
+		}
+		
+		//WORRY i : пометить i - го человека с начала очереди(в нумерации с 0) как беспокоящегося;
+		else if (operation_code == "WORRY") {
+			int i;
+			std::cin >> i;
+			query[i] = true;
+		}
 
-    return 0;
+		//QUIET i : пометить i - го человека(в нумерации с 0) как успокоившегося;
+		else if (operation_code == "QUIET") {
+			int i;
+			std::cin >> i;
+			query[i] = false;
+		}
+
+		//COME k : добавить k спокойных человек в конец очереди;
+		//COME - k: убрать k человек из конца очереди;
+		else if (operation_code == "COME") {
+			int k;
+			std::cin >> k;
+			if (k != 0) 
+				query.resize(query.size() + k);
+			
+		}
+		
+		else {
+
+			// обработка остальных запросов
+
+		}
+	}
+	return 0;
 }
