3015a3acbec3a233c35ff5445184a61f444cf9c0
Sergey <enzzomatrix@gmail.com>
2021-06-29 13:11:57 +1100

    upd

---
+++ Course_03/Sprint_08/01_parallel_algorithms/08_t1_search_engine_parallel_request/08_t1_search_engine_parallel_request/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "search_server.h"
+
+using namespace std::literals;
+
+int main() {
+    SearchServer search_server("and with"s);
+
+    int id = 0;
+    for (
+        const std::string& text : {
+            "funny pet and nasty rat"s,
+            "funny pet with curly hair"s,
+            "funny pet and not very nasty rat"s,
+            "pet with rat and rat and rat"s,
+            "nasty rat with curly hair"s,
+        }
+        ) {
+        search_server.AddDocument(++id, text, DocumentStatus::ACTUAL, { 1, 2 });
+    }
+
+    const std::vector<std::string> queries = {
+        "nasty rat -not"s,
+        "not very funny nasty pet"s,
+        "curly hair"s
+    };
+    id = 0;
+    for (
+        const auto& documents : ProcessQueries(search_server, queries)
+        ) {
+        std::cout << documents.size() << " documents for query ["s << queries[id++] << "]"s << std::endl;
+    }
+
+    return 0;
+}
