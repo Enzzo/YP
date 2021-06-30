e963e69a3c9334af6705affbbdbdee6369e810fb
Sergey <enzzomatrix@gmail.com>
2021-06-30 13:27:14 +1100

    upd

--- include/string_processing.cpp
+++ include/string_processing.cpp
@@ -31,8 +31,11 @@ std::vector<std::string_view> SplitIntoWords(const std::string_view& text) {
     while (true) {
         pos = text.find(' ', pos + 1);
         if (pos == std::string_view::npos) break;
+
+        std::string_view temp(new char[pos]);// = text.substr(pos);
+        //std::move(text.begin(), text.begin() + pos, temp.begin());
         const std::string s = "fff";
-        words.push_back(std::move(const_cast<std::string_view*>()));
+        words.push_back(temp);
         //std::string_view& temp = *words.end();
     }
     int x = 0;
