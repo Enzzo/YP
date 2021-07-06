14bafef1f5bf27bb344221c081163de5b061b83a
Sergey <enzzomatrix@gmail.com>
2021-07-06 11:37:57 +1100

    upd

--- Course_03/Sprint_08/03_race_status_and_protection_against_it/04_critical_area/04_critical_area/main.cpp
+++ Course_03/Sprint_08/03_race_status_and_protection_against_it/04_critical_area/04_critical_area/main.cpp
@@ -1,48 +1,45 @@
+#include <algorithm>
+#include <execution>
+#include <mutex>
+#include <vector>
 #include <iostream>
 #include <numeric>
-#include <vector>
-#include <mutex>
-#include <future>
-#include <execution>
 
-using namespace std::literals;
+using namespace std;
+
 
 template <typename Container, typename Predicate>
-std::vector<typename Container::value_type> CopyIfUnordered(const Container& container, Predicate predicate) {
-    std::vector<typename Container::value_type> result;
-    
-    auto first = container.begin();
-    auto last = container.end();
-    auto mid = container.begin();
-    std::advance(mid, std::distance(container.begin(), container.end()) / 2);
-
-    std::vector<typename Container::value_type> c1(first, mid);
-    std::vector<typename Container::value_type> c2(mid, last);
-
-    std::vector<std::mutex> mtx(container.size());
-    auto it = mtx.begin();
-
-    std::future<void> f1 = std::async([&it, &container, &result, &predicate] {for (const auto& value : container) {
-        
-        if (predicate(value)) {            
-            result.push_back(value);
+vector<typename Container::value_type> CopyIfUnordered(const Container& container, Predicate predicate) {
+    vector<typename Container::value_type> result;
+    result.reserve(container.size());
+    mutex result_mutex;
+    for_each(
+        execution::par,
+        container.begin(), container.end(),
+        [predicate, &result_mutex, &result](const auto& value) {
+            if (predicate(value)) {
+                typename Container::value_type* destination;
+                {
+                    lock_guard guard(result_mutex);
+                    destination = &result.emplace_back();
+                }
+                *destination = value;
+            }
         }
-    }
-    });    
-    f1.get();
-    //std::copy_if(std::execution::par, container.begin(), container.end(), result.begin(), predicate);
+    );
     return result;
 }
 
-int main() {
-    std::vector<int> numbers(1'000);
-    std::iota(numbers.begin(), numbers.end(), 0);
 
-    const std::vector<int> even_numbers =
+int main() {
+    vector<int> numbers(1'000);
+    iota(numbers.begin(), numbers.end(), 0);
+
+    const vector<int> even_numbers =
         CopyIfUnordered(numbers, [](int number) { return number % 2 == 0; });
     for (const int number : even_numbers) {
-        std::cout << number << " "s;
+        cout << number << " "s;
     }
-    std::cout << std::endl;
+    cout << endl;
     // выведет все чётные числа от 0 до 999
 }
---
+++ Course_03/Sprint_08/03_race_status_and_protection_against_it/05_atomic/05_atomic/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <numeric>
+#include <vector>
+#include <mutex>
+#include <future>
+#include <execution>
+
+using namespace std::literals;
+
+template <typename Container, typename Predicate>
+std::vector<typename Container::value_type> CopyIfUnordered(const Container& container, Predicate predicate) {
+    std::vector<typename Container::value_type> result;
+    result.reserve(container.size());
+    std::atomic_int size = 1;
+
+    std::for_each(
+        std::execution::par,
+        container.begin(), container.end(),
+        [predicate, &size, &result](const auto& value) {
+            if (result.size() < size) {
+                if (predicate(value)) {
+                    result.resize(size);
+                    result[size - 1] = value;
+                    size++;
+                }
+            }
+        }
+    );
+    return result;
+}
+
+int main() {
+    std::vector<int> numbers(1'000);
+    std::iota(numbers.begin(), numbers.end(), 0);
+
+    const std::vector<int> even_numbers =
+        CopyIfUnordered(numbers, [](int number) { return number % 2 == 0; });
+    for (const int number : even_numbers) {
+        std::cout << number << " "s;
+    }
+    std::cout << std::endl;
+    // выведет все чётные числа от 0 до 999
+}
