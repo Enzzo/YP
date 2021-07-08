80c437cc289e3dd20609b38f585df37cf1ea52e5
Sergey <enzzomatrix@gmail.com>
2021-07-08 16:12:46 +1100

    upd

--- include/process_queries.cpp
+++ include/process_queries.cpp
@@ -21,3 +21,22 @@ std::vector<Document> ProcessQueriesJoined(const SearchServer& search_server, co
 	}
 	return result;
 }
+
+std::vector<std::vector<Document>> ProcessQueries(const SearchServer& search_server, const std::vector<std::string>& queries) {
+	std::vector<std::vector<Document>> result(queries.size());
+
+	std::transform(std::execution::par, queries.begin(), queries.end(), result.begin(), [&search_server](const std::string_view& request) {
+		return search_server.FindTopDocuments(request);
+		});
+
+	return result;
+}
+
+std::vector<Document> ProcessQueriesJoined(const SearchServer& search_server, const std::vector<std::string>& queries) {
+
+	std::vector<Document> result;
+	for (auto v : ProcessQueries(search_server, queries)) {
+		std::copy(v.begin(), v.end(), std::back_inserter(result));
+	}
+	return result;
+}
