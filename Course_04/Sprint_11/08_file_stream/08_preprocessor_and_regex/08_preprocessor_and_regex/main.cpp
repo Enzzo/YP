852932767972635bb2b5a3b6f5ccf445e743a468
Sergey <enzzomatrix@gmail.com>
2021-08-27 08:24:22 +1100

    upd

--- Course_04/Sprint_11/08_file_stream/08_preprocessor_and_regex/08_preprocessor_and_regex/main.cpp
+++ Course_04/Sprint_11/08_file_stream/08_preprocessor_and_regex/08_preprocessor_and_regex/main.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <string_view>
 #include <vector>
+#include <optional>
 
 using namespace std;
 using filesystem::path;
@@ -15,24 +16,82 @@ path operator""_p(const char* data, std::size_t sz) {
     return path(data, data + sz);
 }
 
+std::optional<std::filesystem::path> FindIncludeFile(const std::filesystem::path& filename, const std::filesystem::path& catalog) {
+    for (const std::filesystem::path& c : std::filesystem::directory_iterator(catalog)) {
+        //если это файл, то возвращаем
+        //если это каталог, то входим рекурсивно
+        std::filesystem::file_status status = std::filesystem::status(c);
+        if (status.type() == std::filesystem::file_type::regular) {
+            const std::filesystem::path file_name = filename;
+            if (c.filename() == file_name.filename()) {
+                return c;
+            }
+        }
+        else {
+            return FindIncludeFile(filename, c);
+        }
+    }
+    return nullopt;
+}
+
 // напишите эту функцию
 bool Preprocess(const path& in_file, const path& out_file, const vector<path>& include_directories) {
+
     std::ifstream in(in_file);
-    std::ofstream out(out_file);
-    
-    //PARSE "in_file"
-    // std::string line;
-    //if(line == #include "..."){
-    //      seek for ... and open and parse
-    //}
-    // if(line == #include <...>){
-    //      check "include_directories"
-    //      
-    // }
-    //else{
-    //  out<<line<<std::endl;
-    //}
-    return false;
+    std::ofstream out(out_file, std::ios::app);
+
+    if (!in.is_open()) {
+        return false;
+    }
+
+    static std::regex relatively(R"/(\s*#\s*include\s*"([^"]*)"\s*)/");
+    static std::regex absolutely(R"/(\s*#\s*include\s*<([^>]*)>\s*)/");
+    std::smatch m;
+
+     std::string line;
+
+     unsigned int row = 0;
+     while (getline(in, line)) {
+         row++;
+
+        if(std::regex_match(line, m, relatively)){
+            //const std::filesystem::path p = in_file.parent_path().string() + "\\" + m[1].str();
+            const std::filesystem::path p = in_file.parent_path() / m[1].str();
+            if (!Preprocess(p, out_file, include_directories)) {
+                bool preprocessed = false;
+                for (const std::filesystem::path& directory : include_directories) {
+                    const std::optional<std::filesystem::path> i = FindIncludeFile(m[1].str(), directory);
+                    if (i) {
+                        preprocessed = Preprocess(i.value(), out_file, include_directories);
+                        break;
+                    }
+                }
+                if (!preprocessed) {
+                    std::cout << "unknown include file \"" << m[1].str() << "\" at file " << in_file << " at line " << row << std::endl;
+                    return false;
+                }
+            }
+        }
+        else if(std::regex_match(line, m, absolutely)){
+            for (const std::filesystem::path& directory : include_directories) {
+                const std::optional<std::filesystem::path> p = FindIncludeFile(m[1].str(), directory);
+                if(p){
+                    if (Preprocess(p.value(), out_file, include_directories)) {
+                        break;
+                    }
+
+                }
+                else {
+                    std::cout << "unknown include file <" << m[1].str() << "> at file " << in_file << " at line " << row << std::endl;
+                    return false;
+                }
+            }
+        }
+        else{
+            out<<line<<std::endl;
+        }
+     }
+    return true;
 }
 
 string GetFileContents(string file) {
@@ -88,11 +147,8 @@ void Test() {
         file << "// std2\n"sv;
     }
 
-    /*assert((!Preprocess("sources"_p / "a.cpp"_p, "sources"_p / "a.in"_p,
-        { "sources"_p / "include1"_p,"sources"_p / "include2"_p })));*/
-
-    Preprocess("sources"_p / "a.cpp"_p, "sources"_p / "a.in"_p,
-        { "sources"_p / "include1"_p,"sources"_p / "include2"_p });
+    assert((!Preprocess("sources"_p / "a.cpp"_p, "sources"_p / "a.in"_p,
+        { "sources"_p / "include1"_p,"sources"_p / "include2"_p })));
 
     ostringstream test_out;
     test_out << "// this comment before include\n"
@@ -109,7 +165,7 @@ void Test() {
         "int SayHello() {\n"
         "    cout << \"hello, world!\" << endl;\n"sv;
 
-    //assert(GetFileContents("sources/a.in"s) == test_out.str());
+    assert(GetFileContents("sources/a.in"s) == test_out.str());
 }
 
 int main() {
