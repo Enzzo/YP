98c36d0ca380050f362f597528ff9f4e62b565fe
Sergey <enzzomatrix@gmail.com>
2021-09-02 17:46:41 +1100

    upd

--- include/transport_directory/json_builder.cpp
+++ include/transport_directory/json_builder.cpp
@@ -1,27 +1,80 @@
 #include "json_builder.h"
 
+using namespace std::literals;
+
 namespace json {
 	Builder& Builder::StartDict() {
-		
+		if (UnableUseStartDict()) {
+			throw std::logic_error("StartDict can't be applied"s);
+		}
+		nodes_stack_.push_back(std::make_unique<Node>(Dict()));
 		return *this;
 	};
+
 	Builder& Builder::EndDict() {
-		return *this;
+		if (UnableUseEndDict()) {
+			throw std::logic_error("EndDict can't be applied"s);
+		}
+		return AddNode(*nodes_stack_.back().release());
 	};
+
 	Builder& Builder::StartArray() {
 		return *this;
 	};
+
 	Builder& Builder::EndArray() {
 		return *this;
 	};
-	Builder& Builder::Key(const Node& node) {
+	
+	Builder& Builder::Key(const std::string& node) {
 		return *this;
 	};
+	
 	Builder& Builder::Value(const Node& node) {
-		ist.
+		nodes_stack_.push_back(std::make_unique<Node>(node));
+		node_ = node;
 		return *this;
 	};
-	Document Builder::Build() {
-		return document_;
+	
+	Node Builder::Build() {
+		return node_;
 	};
+
+	Builder& Builder::AddNode(const Node& node) {
+		nodes_stack_.pop_back();
+		if (nodes_stack_.empty()) {
+			node_ = node;
+		}
+		else if (nodes_stack_.back()->IsArray()) {
+			nodes_stack_.back()->AsArray().push_back(node);
+		}
+		else {
+			const Node& key = *nodes_stack_.back().release();
+			nodes_stack_.pop_back();
+			nodes_stack_.back()->AsDict().emplace(key.AsString(), node);
+		}
+	}
+
+	bool Builder::UnableAdd()const {
+		return !(nodes_stack_.empty()) || nodes_stack_.back()->IsArray() || nodes_stack_.back()->IsString();
+	}
+	bool Builder::MakingNode() const {
+		return !node_.IsNull();
+	}
+
+	bool Builder::UnableUseValue() const {
+		return MakingNode() || UnableAdd();
+	}
+
+	bool Builder::UnableUseStartDict() const {
+		return UnableUseValue();
+	}
+
+	bool Builder::UnableUseEndDict() const {
+		return MakingNode() || nodes_stack_.empty() || !nodes_stack_.back()->IsDict();
+	}
+
+	bool Builder::UnableUseBuild() const {
+		return !MakingNode();
+	}	
 }
--- include/transport_directory/json_builder.h
+++ include/transport_directory/json_builder.h
@@ -7,17 +7,24 @@
 namespace json {
 	class Builder {
 		Node node_;
-		Document document_;
+		std::vector<std::unique_ptr<Node>> nodes_stack_;
 
 	public:
 		Builder& StartDict();
 		Builder& EndDict();
 		Builder& StartArray();
 		Builder& EndArray();
-		Builder& Key(const Node&);
+		Builder& Key(const std::string&);
 		Builder& Value(const Node&);
+		Node Build();
 
-		//создаёт документ
-		Document Build();
+	private:
+		Builder& AddNode(const Node&);
+		bool UnableAdd() const;
+		bool MakingNode() const;
+		bool UnableUseValue() const;
+		bool UnableUseStartDict() const;
+		bool UnableUseEndDict() const;
+		bool UnableUseBuild() const;		
 	};
 }
