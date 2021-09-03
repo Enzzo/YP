8db5e5544e1308643a958e5cfb7d6275e1a3e9ff
Sergey <enzzomatrix@gmail.com>
2021-09-03 15:43:26 +1100

    upd

--- Course_04/Sprint_10/03_inheritance_and_polymorphism/13_transport_catalogue_JSON/13_transport_catalogue_JSON/main.cpp
+++ Course_04/Sprint_10/03_inheritance_and_polymorphism/13_transport_catalogue_JSON/13_transport_catalogue_JSON/main.cpp
@@ -12,15 +12,18 @@ int main() {
 	RequestHandler request(base, renderer);
 	JSONreader reader(base, renderer, request);
 
-	const json::Document doc = json::Load(std::cin);
-	const auto& mode = doc.GetRoot().AsMap();
+	std::ifstream in("C:\\input05.txt");
+	std::ofstream o("out.txt");
+	//const json::Document doc = json::Load(std::cin);
+	const json::Document doc = json::Load(in);
+	const auto& mode = doc.GetRoot().AsDict();
 	
 	if (mode.count("base_requests") || mode.count("render_settings")) {
 		reader.ReadRequest(doc);
 	}
 	if (mode.count("stat_requests")) {
 		reader.ReadRequests(doc);
-		reader.ReadTransportCatalogue();
+		reader.ReadTransportCatalogue(o);
 
 		std::ofstream out("out.svg");
 		request.RenderMap(out);
