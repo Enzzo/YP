a97e80331a1fee8ee40eb8d28efe39e566aa4cb2
Sergey <enzzomatrix@gmail.com>
2021-09-15 11:28:15 +1100

    upd

--- Course_04/Sprint_12/02_data_to_functions/10_forbidden_domains/10_forbidden_domains/main.cpp
+++ Course_04/Sprint_12/02_data_to_functions/10_forbidden_domains/10_forbidden_domains/main.cpp
@@ -4,10 +4,12 @@
 #include <sstream>
 #include <string_view>
 #include <vector>
+#include <unordered_set>
 
 using namespace std;
 
 class Domain {
+    std::string domain_;
     // разработайте класс домена
 
     // конструктор должен позволять конструирование из string, с сигнатурой определитесь сами
@@ -15,18 +17,40 @@ class Domain {
     // разработайте operator==
 
     // разработайте метод IsSubdomain, принимающий другой домен и возвращающий true, если this его поддомен
+public:
+    void SetDomain(std::istream& in) {
+        in >> domain_;
+    }
+private:
 };
+
+std::istream& operator>>(std::istream& in, Domain& domain) {
+    domain.SetDomain(in);
+    return in;
+}
 
 class DomainChecker {
 public:
     // конструктор должен принимать список запрещённых доменов через пару итераторов
+    template<typename It, typename domain = Domain>
+    DomainChecker(It first, It last) {
+        
+    }
 
     // разработайте метод IsForbidden, возвращающий true, если домен запрещён
+    bool IsForbidden(const Domain& checked_domain) {
+        return true;
+    }
 };
 
 // разработайте функцию ReadDomains, читающую заданное количество доменов из стандартного входа
-std::vector<Domain> ReadDomains(std::istream& in, const size_t domains_count) {
 
+std::vector<Domain> ReadDomains(std::istream& in, const size_t domains_count) {
+    std::vector<Domain> domains(domains_count);
+    for (size_t i = 0; i < domains_count; ++i) {
+        in >> domains[i];
+    }    
+    return domains;
 }
 
 template <typename Number>
@@ -43,7 +67,7 @@ Number ReadNumberOnLine(istream& input) {
 int main() {
     const std::vector<Domain> forbidden_domains = ReadDomains(cin, ReadNumberOnLine<size_t>(cin));
     DomainChecker checker(forbidden_domains.begin(), forbidden_domains.end());
-
+    
     const std::vector<Domain> test_domains = ReadDomains(cin, ReadNumberOnLine<size_t>(cin));
     for (const Domain& domain : test_domains) {
         cout << (checker.IsForbidden(domain) ? "Bad"sv : "Good"sv) << endl;
