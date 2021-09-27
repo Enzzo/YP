c3178eff50a5752f07dc226c92055f9d175821f6
Sergey <enzzomatrix@gmail.com>
2021-09-27 17:29:59 +1100

    upd

--- Course_05/Sprint_13/01_undefined_bahavior/03_optimizations/03_optimizations/safe_add.h
+++ Course_05/Sprint_13/01_undefined_bahavior/03_optimizations/03_optimizations/safe_add.h
@@ -9,12 +9,27 @@ template<typename T>
 std::optional<T> SafeAdd(const T l, const T r) {
     if ((l >= 0 && r <= 0) || (l <= 0 && r >= 0)) return l + r;
 
-    const T MAX = std::numeric_limits<T>::max();
-    const T MIN = std::numeric_limits<T>::min();
+    constexpr T MAX = std::numeric_limits<T>::max();
+    constexpr T MIN = std::numeric_limits<T>::min();
 
-    if (MAX < (l + r) ||
-        MIN >(l + r))
-        return std::nullopt;
+    if (l < 0) {
+        if (l > (MAX - r)) {
+            return std::nullopt;
+        }
+    }
+    else {
+        if (l > (MIN + r))
+            return std::nullopt;
+    }
     
+
+    //bool b1 = MAX < (l + r);
+    //bool b2 = MIN > (l + r);
+
+    //bool b1 = l > (MAX - r);
+    //bool b2 = MIN > (l + r);
+
+    //if (b1 || b2) return std::nullopt;
+
     return l + r;
 }
