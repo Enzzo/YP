eefc4d2fd7f649fc42e14d009319e957b45c896e
Enzzo <enzzomatrix@gmail.com>
2021-10-01 15:47:00 +1100

    upd

--- include/containers/vector.h
+++ include/containers/vector.h
@@ -22,13 +22,12 @@ public:
 		, capacity_(capacity){}
 
 	RawMemory(RawMemory&& other) noexcept {
-		capacity_ = other.capacity_;
-		buffer_ = std::move(other.buffer_);		
+		Swap(other);
 	}
 
 	RawMemory& operator=(RawMemory&& rhs) noexcept {
-		capacity_ = std::move(rhs.capacity_);
-		buffer_ = std::move(rhs.buffer_);
+		Swap(rhs);
+		return *this;
 	}
 
 	~RawMemory() {
@@ -36,21 +35,18 @@ public:
 	}
 
 	T* operator+(size_t offset) noexcept {
-		//Разрешается получать адрес ячейки памяти, следующей за последним элементом массива
-		assert(offset <= capacity_);
 		return buffer_ + offset;
 	}
 
 	const T* operator+(size_t offset) const noexcept {
-		return const_cast<RawMemory&>(*this) + offset;
-	}
-
-	const T& operator[](size_t index) const noexcept {
-		return const_cast<RawMemory&>(*this)[index];
+		return buffer_ + offset;
 	}
 
 	T& operator[](size_t index) noexcept {
-		assert(index < capacity_);
+		return buffer_[index];
+	}
+
+	const T& operator[](size_t index) const noexcept {
 		return buffer_[index];
 	}
 
@@ -92,6 +88,7 @@ class Vector {
 public:
 	Vector() = default;
 
+	//------------------------------ctor------------------------------
 	explicit Vector(size_t size)
 		: data_(size)
 		, size_(size) //
@@ -106,25 +103,35 @@ public:
 		std::uninitialized_copy_n(other.data_.GetAddress(), size_, data_.GetAddress());
 	}
 
-	Vector(Vector&& other) {
-		size_ = std::move(other.size_);
-		data_(std::move(other.data_));
+	Vector(Vector&& other) noexcept {
+		Swap(other);
 	}
 
+	//------------------------------assign------------------------------
 	Vector& operator=(const Vector& rhs) {
-		if (this != &rhs) {
-			if (rhs.size_ > data_.Capacity()) {
-				//copy-and-swap
+		if (rhs.size_ > data_.Capacity()) {
+			//copy-and-swap
+			Vector temp(rhs);
+			Swap(temp);
+		}
+		else {
+			for (size_t i = 0; i < std::min(Size(), rhs.Size()); ++i) {
+				data_[i] = rhs[i];
 			}
-			else {
-				//скопировать элементы из rhs, создав при необходимости новые
-				//или удалив существующие
+			if (Size() > rhs.Size()) {
+				std::destroy_n(data_ + rhs.Size(), Size() - rhs.Size());
+			}
+			else if(Size() < rhs.Size()){ //if Size() <= rhs.Size()
+				std::uninitialized_copy_n(rhs.data_.GetAddress() + Size(), rhs.Size() - Size(), data_.GetAddress() + Size());
 			}
 		}
+		size_ = rhs.Size();
+		
 		return *this;
 	}
 
-	Vector& operator=(Vector&& rhs) {
+	Vector& operator=(Vector&& rhs) noexcept{
+		Swap(rhs);
 		return *this;
 	}
 
@@ -133,24 +140,67 @@ public:
 		std::destroy_n(data_.GetAddress(), size_);
 	}
 
-	//------------------------------Reserve------------------------------
+	//------------------------------methods------------------------------
+	//void Reserve(size_t new_capacity) {
+
+	//	if (new_capacity <= data_.Capacity()) {
+	//		return;
+	//	}
+
+	//	RawMemory<T> new_data(new_capacity);
+
+	//	if constexpr (std::is_nothrow_move_constructible_v<T> || !std::is_copy_constructible_v<T>) {
+	//		std::uninitialized_move_n(data_.GetAddress(), size_, new_data.GetAddress());
+	//	}
+	//	else {
+	//		std::uninitialized_copy_n(data_.GetAddress(), size_, new_data.GetAddress());
+	//	}
+
+	//	std::destroy_n(data_.GetAddress(), size_);
+	//	data_.Swap(new_data);
+	//}
+
 	void Reserve(size_t new_capacity) {
-
-		if (new_capacity <= data_.Capacity()) {
-			return;
+		if (new_capacity > Capacity()) {
+			RawMemory<T> temp(new_capacity);
+ 			std::uninitialized_move_n(data_.GetAddress(), size_, temp.GetAddress());
+			std::destroy_n(data_.GetAddress(), size_);
+			data_.Swap(temp);
 		}
+	}
 
-		RawMemory<T> new_data(new_capacity);
+	void Resize(size_t new_size) {   
+		Reserve(new_size);
 
-		if constexpr (std::is_nothrow_move_constructible_v<T> || !std::is_copy_constructible_v<T>) {
-			std::uninitialized_move_n(data_.GetAddress(), size_, new_data.GetAddress());
+		if (new_size < size_) {
+			std::destroy_n(data_ + new_size, size_ - new_size);
 		}
-		else {
-			std::uninitialized_copy_n(data_.GetAddress(), size_, new_data.GetAddress());
+		else if (new_size > size_) {
+			std::uninitialized_default_construct_n(data_ + size_, new_size - size_);
 		}
+		size_ = new_size;
+	}
 
-		std::destroy_n(data_.GetAddress(), size_);
-		data_.Swap(new_data);
+	
+	void PushBack(const T& value) {
+
+		if (size_ == data_.Capacity()) {
+			Reserve(size_ == 0 ? 1 : size_ * 2);
+		}
+		new (data_.GetAddress() + size_) T(value);
+		++size_;
+	}
+
+	void PushBack(T&& value) {
+		if (size_ == data_.Capacity()) {
+			Reserve(size_ == 0 ? 1 : size_ * 2);
+		}
+		new (data_.GetAddress() + size_) T(std::move(value));
+		++size_;
+	}
+
+	void PopBack() noexcept {
+		std::destroy_at(data_ + --size_);
 	}
 
 	size_t Size() const noexcept {
@@ -161,14 +211,21 @@ public:
 		return data_.Capacity();
 	}
 
+	void Swap(Vector& other) noexcept {
+		data_.Swap(other.data_);
+		std::swap(size_, other.size_);		
+	}
+
+	//------------------------------index------------------------------
 	const T& operator[](size_t index) const noexcept {
-		return const_cast<Vector&>(*this)[index];
+		return data_[index];
 	}
 
 	T& operator[](size_t index) noexcept {
-		assert(index < size_);
 		return data_[index];
 	}
+
+	
 
 private:
 	//Вызывает деструкторы n объектов массива по адресу buf
@@ -186,10 +243,5 @@ private:
 	//Вызывает деструктор объекта по адресу buf
 	static void Destroy(T* buf) noexcept {
 		buf->~T();
-	}
-
-	void Swap(Vector& other) noexcept {
-		std::swap(size_, other.size_);
-		std::swap(data_, other.data_);
-	}
+	}	
 };
