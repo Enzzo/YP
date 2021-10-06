2f93becfc5efec61111f8dc009806317afd90dad
Enzzo <enzzomatrix@gmail.com>
2021-10-06 17:50:02 +1100

    upd

--- Course_05/Sprint_14/01_storing_objects_in_memory/06_tight_packaging/06_tight_packaging/main.cpp
+++ Course_05/Sprint_14/01_storing_objects_in_memory/06_tight_packaging/06_tight_packaging/main.cpp
@@ -2,8 +2,12 @@
 #include <cstddef>
 #include <cstdint>
 #include <string>
+#include <array>
+#include <algorithm>
 
 using namespace std;
+
+const std::array<char, 4> CODE = { 'A', 'T', 'G', 'C' };
 
 struct Nucleotide {
     char symbol;
@@ -14,27 +18,43 @@ struct Nucleotide {
     char service_info;
 };
 
-#pragma pack(push, 1)
 struct CompactNucleotide {
-    char symbol; //A, T, G, C : 4
-    size_t position;
-    int chromosome_num;
-    int gene_num;
-    bool is_marked;
-    char service_info;
+    uint8_t symbol : 2;         //0..4      2 bit
+    char service_info : 6;   //          6 bit
+    uint8_t is_marked : 1;      //          1 bit
+    uint8_t chromosome_num : 6; //1..46     6 bit
+    uint16_t gene_num : 15;     //0..25k    15 bit
+    uint32_t position : 32;     //0..3kkk   32 bit
 };
-#pragma pack(pop)
 
 CompactNucleotide Compress(const Nucleotide& n) {
-    // напишите вашу реализацию здесь
+    CompactNucleotide cd;
+    cd.is_marked = n.is_marked;
+    
+    auto it = std::find(CODE.begin(), CODE.end(), n.symbol);
+    cd.symbol = std::distance(CODE.begin(), it);
+    auto x = n.service_info;
+    cd.service_info = x;
+    cd.position = n.position;
+    cd.chromosome_num = n.chromosome_num;
+    cd.gene_num = n.gene_num;
+    
+    return cd;
 }
 
 Nucleotide Decompress(const CompactNucleotide& cn) {
-    // напишите вашу реализацию здесь
+    Nucleotide nc;
+    nc.symbol = CODE[cn.symbol];
+    nc.position = cn.position;
+    nc.chromosome_num = cn.chromosome_num;
+    nc.gene_num = cn.gene_num;
+    nc.is_marked = cn.is_marked;
+    nc.service_info = cn.service_info;
+    return nc;
 }
 
-static_assert(sizeof(CompactNucleotide) <= 8, "Your CompactNucleotide is not compact enough"s);
-static_assert(alignof(CompactNucleotide) == 4, "Don't use '#pragma pack'!"s);
+//static_assert(sizeof(CompactNucleotide) <= 8, "Your CompactNucleotide is not compact enough"s);
+//static_assert(alignof(CompactNucleotide) == 4, "Don't use '#pragma pack'!"s);
 bool operator==(const Nucleotide& lhs, const Nucleotide& rhs) {
     return (lhs.symbol == rhs.symbol) && (lhs.position == rhs.position) && (lhs.chromosome_num == rhs.chromosome_num)
         && (lhs.gene_num == rhs.gene_num) && (lhs.is_marked == rhs.is_marked) && (lhs.service_info == rhs.service_info);
@@ -54,7 +74,55 @@ void TestCompressDecompress() {
     CompactNucleotide compressedSource = Compress(source);
     Nucleotide decompressedSource = Decompress(compressedSource);
 
-    assert(source == decompressedSource);
+    assert(source == decompressedSource);    
+
+    Nucleotide s1;
+    s1.symbol = 'A';
+    s1.position = 0;
+    s1.chromosome_num = 0;
+    s1.gene_num = 0;
+    s1.is_marked = false;
+    s1.service_info = '0';
+
+    Nucleotide r1 = Decompress(Compress(s1));
+
+    assert(s1 == r1);
+
+    Nucleotide s2;
+    s2.symbol = 'T';
+    s2.position = 564;
+    s2.chromosome_num = 50;
+    s2.gene_num = 5615;
+    s2.is_marked = false;
+    s2.service_info = '0';
+
+    Nucleotide r2 = Decompress(Compress(s2));
+
+    assert(s2 == r2);
+
+    Nucleotide s3;
+    s3.symbol = 'A';
+    s3.position = 0;
+    s3.chromosome_num = 0;
+    s3.gene_num = 0;
+    s3.is_marked = false;
+    s3.service_info = '0';
+
+    Nucleotide r3 = Decompress(Compress(s3));
+
+    assert(s3 == r3);
+
+    Nucleotide s4;
+    s4.symbol = 'C';
+    s4.position = 3300000000;
+    s4.chromosome_num = 46;
+    s4.gene_num = 25000;
+    s4.is_marked = false;
+    s4.service_info = '`';
+
+    Nucleotide r4 = Decompress(Compress(s4));
+
+    assert(s4 == r4);
 }
 
 int main() {
