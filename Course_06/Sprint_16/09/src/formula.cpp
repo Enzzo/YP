3e6c789c7acbddf0eeb00833f75df15eb6522692
Enzzo <enzzomatrix@gmail.com>
2021-11-15 11:48:36 +1100

    upd

---
+++ Course_06/Sprint_16/09/src/formula.cpp
@@ -0,0 +1,53 @@
+#include "formula.h"
+
+#include "FormulaAST.h"
+
+#include <algorithm>
+#include <cassert>
+#include <cctype>
+#include <sstream>
+
+using namespace std::literals;
+
+std::ostream& operator<<(std::ostream& output, FormulaError fe) {
+    return output << "#DIV/0!";
+}
+
+namespace {
+    class Formula : public FormulaInterface {
+    public:
+        // Реализуйте следующие методы:
+        explicit Formula(std::string expression)        
+            :ast_(ParseFormulaAST(std::move(expression)))
+        {
+            
+        }
+        Value Evaluate() const override {
+            try {
+                double a = ast_.Execute();
+                return a;
+            }
+            catch (FormulaError& e) {
+                return e;
+            }
+        }
+        std::string GetExpression() const override {
+            std::ostringstream result;
+            ast_.PrintFormula(result);
+            return result.str();
+        }
+
+    private:
+        const FormulaAST ast_;        
+    };
+}  // namespace
+
+std::unique_ptr<FormulaInterface> ParseFormula(std::string expression) {
+    //TODO: если строка expression содержит синтаксически некорректную формулу, то метод должен кинуть исключение FormulaException
+    try {
+        return std::make_unique<Formula>(std::move(expression));
+    }
+    catch (...) {
+        throw FormulaException("");
+    }
+}
